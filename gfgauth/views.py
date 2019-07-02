06df106b6b2bd6ba910aa24f0659cbc09eea3745
Ayush Tripathi <ayush.me001@gmail.com>
2019-07-02 15:30:58 +0530

    added updates

--- gfgauth/views.py
+++ gfgauth/views.py
@@ -26,44 +26,46 @@ def home(request):
     storage = DjangoORMStorage(CredentialsModel, 'id', request.user, 'credential')
     print(storage)
     credential = storage.get()
-    try:
-        access_token = credential.access_token
-        #resp, cont = Http().request("https://www.googleapis.com/auth/gmail.readonly", headers={'Host': 'www.googleapis.com','Authorization': access_token})
-        #resp, cont = Http().request("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", headers = {'Host': 'www.googleapis.com', 'Authorization': access_token})
-        URL = "https://www.googleapis.com/plus/v1/people/"
-        PARAMS = {'userId': 'me'}
-        r = requests.get(url = URL, params = PARAMS)
-        data = r.json()
-        #print("hellllllo====", data)
-    except:
+
+    if credential is None or credential.invalid:
         status = False
         print('Not Found')
-
-    return render(request, 'index.html', {'status': status})
+    else:
+        access_token = credential.access_token
+        http = httplib2.Http()
+        http = credential.authorize(http)
+        people_document = "hello world"
+        service = build('plus', 'v1', http=http)
+        people_resource = service.people()
+        people_document = people_resource.get(userId='me').execute()
+        print(people_document)
+        #print("hellllllo====", data)
+    #return HttpResponseRedirect("gmail_authenticate")
+    return render(request, 'index.html', {'status': status, 'people_document': people_document})
+    #return render(request, 'index.html', {'status': status`})
 
 
 ################################
 #   GMAIL API IMPLEMENTATION   #
 ################################
 
-# CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
-# application, including client_id and client_secret, which are found
-# on the API Access tab on the Google APIs
-# Console <http://code.google.com/apis/console>
 
 
 FLOW = flow_from_clientsecrets(
     settings.GOOGLE_OAUTH2_CLIENT_SECRETS_JSON,
     #scope='https://www.googleapis.com/auth/gmail.readonly',
-    scope='https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+    scope='https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
     redirect_uri='http://127.0.0.1:8000/oauth2callback',
     prompt='consent')
+
+
 
 
 def gmail_authenticate(request):
     storage = DjangoORMStorage(CredentialsModel, 'id', request.user, 'credential')
     credential = storage.get()
     if credential is None or credential.invalid:
+        print("helo========\n")
         FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
                                                        request.user)
         authorize_url = FLOW.step1_get_authorize_url()
@@ -71,6 +73,7 @@ def gmail_authenticate(request):
     else:
         http = httplib2.Http()
         http = credential.authorize(http)
+        people_document = "hello world"
         service = build('plus', 'v1', http=http)
         people_resource = service.people()
         people_document = people_resource.get(userId='me').execute()
@@ -81,9 +84,10 @@ def gmail_authenticate(request):
         print(people_document)
         print('access_token = ', credential.access_token)
         status = True
-
         return render(request, 'index.html', {'status': status, 'people_document': people_document })
 
+
+#FOR FIRST TIME AUTHENTICATION
 
 def auth_return(request):
     get_state = bytes(request.GET.get('state'), 'utf8')
